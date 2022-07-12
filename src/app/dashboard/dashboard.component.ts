a440ba2db3566401dafd2475d8fe474834bc9f9c
metardn <meta.arda.tif419@polban.ac.id>
2022-07-12 20:58:15 +0700

    update dashboard page add chart

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EChartsOption } from 'echarts';
 import { SharedService } from '../shared/shared.service';
 
 @Component({
@@ -8,6 +9,75 @@ import { SharedService } from '../shared/shared.service';
 })
 export class DashboardComponent implements OnInit {
   listings : any
+
+  option1 : EChartsOption = {
+    color: [
+      '#E6FBD9',
+      '#44C13C'
+    ],
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['80%', '95%'],
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: 'center'
+        },
+        data: [
+          { value: 4 },
+          { value: 8 }
+        ]
+      }
+    ]
+  }
+
+  option2 : EChartsOption = {
+    color: [
+      '#D8D8FE',
+      '#4040F2'
+    ],
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['80%', '95%'],
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: 'center'
+        },
+        data: [
+          { value: 4 },
+          { value: 8 }
+        ]
+      }
+    ]
+  }
+
+  option3 : EChartsOption = {
+    color: [
+      '#FF513A',
+      '#FFE8D7'
+    ],
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['80%', '95%'],
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: 'center'
+        },
+        data: [
+          { value: 1 },
+          { value: 1 }
+        ]
+      }
+    ]
+  }
 
   constructor(private shared: SharedService) { }
 
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 
 @NgModule({
@@ -11,7 +12,15 @@ import { DashboardComponent } from './dashboard.component';
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    NgxEchartsModule.forRoot({
+      /**
+       * This will import all modules from echarts.
+       * If you only need custom modules,
+       * please refer to [Custom Build] section.
+       */
+      echarts: () => import('echarts'), // or import('./path-to-my-custom-echarts')
+    }),
   ]
 })
 export class DashboardModule { }
