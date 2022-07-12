bcc5ab68f8ae7f145fe96feda24bfb220ea077f8
metardn <meta.arda.tif419@polban.ac.id>
2022-07-12 14:25:47 +0700

    create listing property page

--- src/app/property-listing/property-listing.component.ts
+++ src/app/property-listing/property-listing.component.ts
@@ -6,7 +6,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-listing.component.scss']
 })
 export class PropertyListingComponent implements OnInit {
+  listings = [
+    {
+      "name": "Boston family home",
+      "desc" : "A beautiful townhouse in the heart of Miami's down town district. With 3 bedrooms and heated pool.",
+      "image" : "assets/listings/img1.jpg",
+      "card" : "card1"
+    },
+    {
+      "name": "Miami  beach house",
+      "desc" : "A beautiful townhouse in the heart of Miami's down town district. With 3 bedrooms and heated pool.",
+      "image" : "assets/listings/img2.jpg",
+      "card" : "card2"
+    },
+    {
+      "name": "New York Townhouse",
+      "desc" : "A beautiful townhouse in the heart of Miami's down town district. With 3 bedrooms and heated pool.",
+      "image" : "assets/listings/img3.jpg",
+      "card" : "card3"
+    }
+  ]
+  images = [
+    {
+      "url" : "assets/mosaic/img1.jpg",
+      "style" : "img1"
+    },
+    {
+      "url" : "assets/mosaic/img2.jpg",
+      "style" : "img2"
+    },
+    {
+      "url" : "assets/mosaic/img3.jpg",
+      "style" : "img3"
+    },
+    {
+      "url" : "assets/mosaic/img4.jpg",
+      "style" : "img4"
+    },
+    {
+      "url" : "assets/mosaic/img5.jpg",
+      "style" : "img5"
+    }
+  ]
 
+  details = [
+    {
+      "label" : "Asking price",
+      "body" : "£999,999",
+      "icon" : "bi bi-currency-dollar"
+    },
+    {
+      "label" : "Contact details",
+      "body" : "+44 020 8479 9242",
+      "icon" : "bi bi-telephone"
+    },
+    {
+      "label" : "Managing agent",
+      "body" : "Christoph Paul",
+      "icon" : "bi bi-person"
+    },
+    {
+      "label" : "Expected returns",
+      "body" : "5% annual profits",
+      "icon" : "bi bi-graph-up-arrow"
+    },
+    {
+      "label" : "Date available",
+      "body" : "21 January 2022",
+      "icon" : "bi bi-calendar"
+    },
+    {
+      "label" : "Property type",
+      "body" : "Residential unit",
+      "icon" : "bi bi-building"
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
