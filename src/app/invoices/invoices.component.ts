4823a1a2b7b4dfc0216c2b9e280d2b7f55012577
metardn <meta.arda.tif419@polban.ac.id>
2022-07-13 12:44:47 +0700

    create invoice page

--- src/app/invoices/invoices.component.ts
+++ src/app/invoices/invoices.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EChartsOption } from 'echarts';
 
 @Component({
   selector: 'app-invoices',
@@ -7,6 +8,140 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoicesComponent implements OnInit {
 
+  option1 : EChartsOption = {
+    color: [
+      '#FF513A',
+      '#FFE8D7'
+    ],
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['80%', '95%'],
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: 'center'
+        },
+        data: [
+          { value: 1 },
+          { value: 1 }
+        ]
+      }
+    ]
+  }
+
+  option2 : EChartsOption = {
+    color: [
+      '#E6FBD9',
+      '#44C13C'
+    ],
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['80%', '95%'],
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: 'center'
+        },
+        data: [
+          { value: 4 },
+          { value: 8 }
+        ]
+      }
+    ]
+  }
+
+  option3 : EChartsOption = {
+    color: [
+      '#E6FBD9',
+      '#44C13C'
+    ],
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['80%', '95%'],
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: 'center'
+        },
+        data: [
+          { value: 4 },
+          { value: 8 }
+        ]
+      }
+    ]
+  }
+
+  status_list = [
+    {
+      "label" : "In review",
+      "type" : "review-badge"
+    },
+    {
+      "label" : "In review",
+      "type" : "review-badge"
+    },
+    {
+      "label" : "Approved",
+      "type" : "approved-badge"
+    },
+    {
+      "label" : "Approved",
+      "type" : "approved-badge"
+    },
+    {
+      "label" : "In review",
+      "type" : "review-badge"
+    },
+    {
+      "label" : "Approved",
+      "type" : "approved-badge"
+    },
+    {
+      "label" : "In review",
+      "type" : "review-badge"
+    },
+    {
+      "label" : "Approved",
+      "type" : "approved-badge"
+    },
+    {
+      "label" : "In review",
+      "type" : "review-badge"
+    },
+    {
+      "label" : "Cancelled",
+      "type" : "cancel-badge"
+    },
+    {
+      "label" : "Approved",
+      "type" : "approved-badge"
+    },
+    {
+      "label" : "Cancelled",
+      "type" : "cancel-badge"
+    },
+    {
+      "label" : "Approved",
+      "type" : "approved-badge"
+    },
+    {
+      "label" : "In review",
+      "type" : "review-badge"
+    },
+    {
+      "label" : "In review",
+      "type" : "review-badge"
+    }
+  ]
+
+  sony = "assets/client/sony.png"
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/invoices/invoices.module.ts
+++ src/app/invoices/invoices.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { InvoicesRoutingModule } from './invoices-routing.module';
 import { InvoicesComponent } from './invoices.component';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 
 @NgModule({
@@ -11,7 +12,15 @@ import { InvoicesComponent } from './invoices.component';
   ],
   imports: [
     CommonModule,
-    InvoicesRoutingModule
+    InvoicesRoutingModule,
+    NgxEchartsModule.forRoot({
+      /**
+       * This will import all modules from echarts.
+       * If you only need custom modules,
+       * please refer to [Custom Build] section.
+       */
+      echarts: () => import('echarts'), // or import('./path-to-my-custom-echarts')
+    }),
   ]
 })
 export class InvoicesModule { }
